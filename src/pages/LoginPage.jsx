e89a594b18ce5272794b26864dae48dbd1bd89b5
chaelinlee <leechelin1234@gmail.com>
2026-05-21 18:31:35 +0900

    feat: 로그인 페이지 생성

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+function LoginPage() {
+  return (
+    <div>
+      <h1>로그인 페이지</h1>
+
+      <input type="text" placeholder="아이디" />
+
+      <input type="password" placeholder="비밀번호" />
+
+      <button>로그인</button>
+    </div>
+  );
+}
+
+export default LoginPage;
